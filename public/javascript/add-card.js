5fba855765bfdaaabc6a6413e1df868ead8baa78
Xeroneon <andrewvasquez0113@gmail.com>
2018-08-18 22:36:55 -0700

    a lot of research and testing random code. got an api and searching working

---
+++ public/javascript/add-card.js
@@ -0,0 +1,61 @@
+$("#search-card").on("click", function() {
+    event.preventDefault();
+
+    console.log("clicked")
+
+    const newCard = {
+        cardName: $("#search-val").val().trim()
+    }
+
+    $.ajax("/api/search-card", {
+        type: "POST",
+        //user the new user object and send it to the route
+        data: newCard
+    }).then(function(res) {
+        console.log(res[0])
+        if(res[0].imageUrl) {
+            var cardImage = $("<img>").attr("src", res[0].imageUrl);
+            cardImage.attr("id", "card-image");
+        }
+
+        cardName = $("<p>").append(res[0].name);
+        cardName.attr("id", "card-name");
+
+        manaCost = $("<p>").append("Mana Cost:" + res[0].manaCost);
+        manaCost.attr("id", "mana-cost");
+
+        cardDesc = $("<p>").append(res[0].text);
+        cardDesc.attr("id", "card-desc");
+
+        addCard = $("<a>").append("Add Card");
+        addCard.attr("class", "waves-effect waves-green btn-flat");
+        addCard.attr("id", "send-card");
+
+        
+        $(".modal-content").append(cardImage);
+        $(".modal-content").append(cardName);
+        $(".modal-content").append(manaCost);
+        $(".modal-content").append(cardDesc);
+        $(".modal-content").append(addCard);
+
+
+    })
+})
+
+$("body").on("click", "#send-card", function() {
+    console.log($("#card-name").text());
+
+    const newCard = {
+        cardName: $("#card-name").text()
+    }
+
+    $.ajax("/api/add-card", {
+        type: "POST",
+        //user the new user object and send it to the route
+        data: newCard
+    }).then(function(res) {
+        console.log(res);
+
+
+    })
+})
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,65 @@
 const db = require("../models");
 
-module.exports = function(app) {
-    app.post("/api/users", function(req, res) {
+const mtg = require('mtgsdk');
+
+module.exports = function (app) {
+    app.post("/api/users", function (req, res) {
         let newUser = new db.User();
         req.body.user_password = newUser.genHash(req.body.user_password);
-        db.User.create(req.body).then(function(results) {
+        db.User.create(req.body).then(function (results) {
             res.json(results);
         })
     })
     //route hit when a user logs in
-    app.post("/api/login", function(req, res) {
+    app.post("/api/login", function (req, res) {
         //use sequelize to find their account by name
-        db.User.findOne({where: {user_name: req.body.user_name}}).then(user => {
+        db.User.findOne({ where: { user_name: req.body.user_name } }).then(user => {
             //then using a method created on the user model authorizes and checks hashed password and if it returns true(correct password) send back that they are authorized
             if (user.Authorize(req.body.user_password)) {
                 //sends back whether the user is authorized
-                res.cookie("user_password", req.body.user_password, {maxAge: 1000 * 60 * 60 * 24});
-                res.cookie("user_name", req.body.user_name, {maxAge: 1000 * 60 * 60 *24});
-                res.json({Auth: true});
+                res.cookie("user_password", req.body.user_password, { maxAge: 1000 * 60 * 60 * 24 });
+                res.cookie("user_name", req.body.user_name, { maxAge: 1000 * 60 * 60 * 24 });
+                res.json({ Auth: true });
             } else {
                 //TODO => create logic for if the password is wrong here
                 console.log("not auth")
             }
         })
+    })
+
+    app.post("/api/search-card", function (req, res) {
+        let cardName = req.body.cardName;
+
+            mtg.card.where({ name: cardName, pageSize: 1 })
+            .then(card => {
+                res.json(card);
+            })
+
+
+    })
+
+    app.post("/api/add-card", function (req, res) {
+        let cardName = req.body.cardName;
+
+            mtg.card.where({ name: cardName, pageSize: 1 })
+            .then(card => {
+                // res.json(card);
+
+                let newCard = {
+                    card_name: card[0].name,
+                    card_description: card[0].text,
+                    card_edition: card[0].set,
+                    card_image: card[0].imageUrl,
+                    card_artist: card[0].artist,
+                    card_mana_cost: card[0].manaCost,
+                }
+
+                db.Card.create(newCard).then(function(results) {
+                    console.log("card Created")
+                    res.json(results)
+                })
+            })
+
+
     })
 };
